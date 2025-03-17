e14923e2a77d1e059b1138fbc42afb9ab9061ea6
qellyka <qellyka@proton.me>
2025-03-17 21:50:08 +0400

    ADD IsAdminCb filter for callback

--- app/filters.py
+++ app/filters.py
@@ -1,14 +1,19 @@
 from aiogram.filters import BaseFilter
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 import app.db.requests as rq
 
 class IsAdmin(BaseFilter):
     async def __call__(self, message: Message) -> bool:
-        user = await rq.get_data(message.from_user.id)
+        user = await rq.get_user_data(message.from_user.id)
+        return user.is_admin
+
+class IsAdminCb(BaseFilter):
+    async def __call__(self, cb: CallbackQuery) -> bool:
+        user = await rq.get_user_data(cb.from_user.id)
         return user.is_admin
 
 class IsUser(BaseFilter):
     async def __call__(self, message: Message) -> bool:
-        user = await rq.get_data(message.from_user.id)
+        user = await rq.get_user_data(message.from_user.id)
         return not(user.is_admin)
