21e3f8dfc0f2e1e77200952beb305ed4dafd3ed8
qellyka <qellyka@proton.me>
2025-03-18 20:06:26 +0400

    EDIT requests

--- app/db/requests.py
+++ app/db/requests.py
@@ -2,7 +2,7 @@ import datetime
 from dataclasses import asdict
 
 from app.db.engine import async_session
-from app.db.models import UserBase, LotBase, Status
+from app.db.models import UserBase, LotBase, LotStatus, LotModStatus
 
 from sqlalchemy import select, BigInteger
 
@@ -18,7 +18,8 @@ async def set_lot(tg_id: BigInteger, starter_price: int, hours_exp: int, photo_i
             moment_buy_price = starter_price + (starter_price / 100 * 20),
             completion_time = datetime.datetime.now() + datetime.timedelta(hours=hours_exp),
             seller = user.username,
-            status = Status.bidding
+            is_post = LotModStatus.PENDING,
+            status = LotStatus.bidding
         ))
         user.lots += 1
         await session.commit()
@@ -46,7 +47,7 @@ async def get_lot_data(lot_id: BigInteger):
 
 async def get_new_lots():
     async with async_session() as session:
-        lots = await session.scalars(select(LotBase).where(LotBase.is_post==False))
+        lots = await session.scalars(select(LotBase).where(LotBase.is_post==LotModStatus.PENDING))
         lots_list = [
             {key: getattr(lot, key) for key in lot.__table__.columns.keys()}
             for lot in lots
@@ -59,18 +60,19 @@ async def get_new_lots():
 async def approve_lot(lot_id: BigInteger):
     async with async_session() as session:
         lot = await session.scalar(select(LotBase).where(LotBase.id==lot_id))
-        lot.is_post = True
+        lot.is_post = LotModStatus.APPROVED
         await session.commit()
 
 async def reject_lot(lot_id: BigInteger, tg_id: BigInteger):
     async with async_session() as session:
         lot = await session.scalar(select(LotBase).where(LotBase.id==lot_id))
+        lot.is_post = LotModStatus.REJECTED
         user = await get_user_data(tg_id)
-        user.lots -= 1
-        await session.delete(lot)
+        session.merge(user)
+        user.lots =  user.lots - 1
         await session.commit()
 
-async def set_new_user(user: UserBase):
+async def set_new_user(user):
     async with async_session() as session:
         user.is_new = False
         await session.commit()
