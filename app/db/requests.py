77d1a2041cce41960e67cb3d8a66373cb09fd976
qellyka <qellyka@proton.me>
2025-03-23 14:50:04 +0400

    ADD requests for next lot, prev. lot, first new lot

--- app/db/requests.py
+++ app/db/requests.py
@@ -9,7 +9,9 @@ from sqlalchemy import select, BigInteger
 
 async def set_lot(tid: BigInteger, starter_price: int, hours_exp: int, pid: str, blitz_price: int):
     async with async_session() as session:
-        user = await session.scalar(select(UserBase).where(UserBase.telegram_id==tid))
+        user = await session.scalar(
+            select(UserBase)
+            .where(UserBase.telegram_id==tid))
         session.add(LotBase(
             user_id =user.id,
             photo_id=pid,
@@ -27,28 +29,38 @@ async def set_lot(tid: BigInteger, starter_price: int, hours_exp: int, pid: str,
 
 async def deposit_balance(tg_id: BigInteger, stars: int):
     async with async_session() as session:
-        user = await session.scalar(select(UserBase).where(UserBase.telegram_id==tg_id))
+        user = await session.scalar(
+            select(UserBase)
+            .where(UserBase.telegram_id==tg_id))
         user.balance += stars
         await session.commit()
 
 async def get_user_data(tg_id: BigInteger):
     async with async_session() as session:
-        user = await session.scalar(select(UserBase).where(UserBase.telegram_id==tg_id))
+        user = await session.scalar(
+            select(UserBase)
+            .where(UserBase.telegram_id==tg_id))
         return  user
 
 async def get_user_data_id(tid: BigInteger):
     async with async_session() as session:
-        user = await session.scalar(select(UserBase).where(UserBase.id==tid))
+        user = await session.scalar(
+            select(UserBase)
+            .where(UserBase.id==tid))
         return  user
 
 async def get_lot_data(lot_id: BigInteger):
     async with async_session() as session:
-        lot = await session.scalar(select(LotBase).where(LotBase.id==lot_id))
+        lot = await session.scalar(
+            select(LotBase)
+            .where(LotBase.id==lot_id))
         return lot
 
 async def get_new_lots():
     async with async_session() as session:
-        lots = await session.scalars(select(LotBase).where(LotBase.is_post==LotModStatus.PENDING))
+        lots = await session.scalars(
+            select(LotBase)
+            .where(LotBase.is_post==LotModStatus.PENDING))
         lots_list = [
             {key: getattr(lot, key) for key in lot.__table__.columns.keys()}
             for lot in lots
@@ -57,48 +69,64 @@ async def get_new_lots():
 
 async def approve_lot(lot_id: BigInteger):
     async with async_session() as session:
-        lot = await session.scalar(select(LotBase).where(LotBase.id==lot_id))
+        lot = await session.scalar(
+            select(LotBase)
+            .where(LotBase.id==lot_id))
         lot.is_post = LotModStatus.APPROVED
         lot.expired_at = datetime.datetime.now(ZoneInfo("Europe/Moscow")).replace(tzinfo=None) + datetime.timedelta(hours=lot.expired_time)
         await session.commit()
 
 async def reject_lot(lot_id: BigInteger, tg_id: BigInteger):
     async with async_session() as session:
-        lot = await session.scalar(select(LotBase).where(LotBase.id==lot_id))
+        lot = await session.scalar(
+            select(LotBase)
+            .where(LotBase.id==lot_id))
         lot.is_post = LotModStatus.REJECTED
         await session.commit()
 
 async def set_new_user(tid : BigInteger):
     async with async_session() as session:
-        user = await session.scalar(select(UserBase).where(UserBase.telegram_id==tid))
+        user = await session.scalar(
+            select(UserBase)
+            .where(UserBase.telegram_id==tid))
         user.is_new = False
         await session.commit()
 
 async def get_user_by_username(username: str):
     async with async_session() as session:
-        user = await session.scalar(select(UserBase).where(UserBase.username==username))
+        user = await session.scalar(
+            select(UserBase)
+            .where(UserBase.username==username))
         return user
 
 async def ban_user(tid: BigInteger):
     async with async_session() as session:
-        user = await session.scalar(select(UserBase).where(UserBase.telegram_id==tid))
+        user = await session.scalar(
+            select(UserBase)
+            .where(UserBase.telegram_id==tid))
         user.is_banned = True
         await session.commit()
 
 async def unban_user(tid: BigInteger):
     async with async_session() as session:
-        user = await session.scalar(select(UserBase).where(UserBase.telegram_id==tid))
+        user = await session.scalar(
+            select(UserBase)
+            .where(UserBase.telegram_id==tid))
         user.is_banned = False
         await session.commit()
 
 async def get_lot_data_by_photo_id(pid: str):
     async with async_session() as session:
-        lot = await session.scalar(select(LotBase).where(LotBase.photo_id==pid))
+        lot = await session.scalar(
+            select(LotBase)
+            .where(LotBase.photo_id==pid))
         return lot
 
 async def get_blocked_users():
     async with async_session() as session:
-        users = await session.scalars(select(UserBase).where(UserBase.is_banned == True))
+        users = await session.scalars(
+            select(UserBase)
+            .where(UserBase.is_banned == True))
         users_list = [
             {key: getattr(user, key) for key in user.__table__.columns.keys()}
             for user in users
@@ -108,14 +136,26 @@ async def get_blocked_users():
 async def get_first_new_lot():
     async with async_session() as session:
         return await session.scalar(
-            select(LotBase).where(LotBase.status == LotModStatus.PENDING).order_by(LotBase.id.asc()).limit(1)
+            select(LotBase)
+            .where(LotBase.is_post == LotModStatus.PENDING)
+            .order_by(LotBase.id.asc())
+            .limit(1)
         )
 
 async def get_next_lot(current_lot_id):
     async with async_session() as session:
         return await session.scalar(
             select(LotBase)
-            .where(LotBase.status == LotModStatus.PENDING, LotBase.id > current_lot_id)
+            .where(LotBase.is_post == LotModStatus.PENDING, LotBase.id > current_lot_id)
             .order_by(LotBase.id.asc())
+            .limit(1)
+        )
+
+async def get_previous_lot(current_lot_id):
+    async with async_session() as session:
+        return await session.scalar(
+            select(LotBase)
+            .where(LotBase.is_post == LotModStatus.PENDING, LotBase.id < current_lot_id)
+            .order_by(LotBase.id.desc())
             .limit(1)
         )
