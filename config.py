b6b289b45d385b26c3c59cb0b864954b4ce98b1f
qellyka <qellyka@proton.me>
2025-03-24 21:20:34 +0300

    ADD status_mapping

--- config.py
+++ config.py
@@ -8,3 +8,10 @@ CHANNEL_ID = os.getenv('CHANNEL_ID')
 TOKEN = os.getenv('BOT_TOKEN')
 DB_URL = os.getenv('DB_URL')
 PAYMENTS_TOKEN = os.getenv('PAYMENTS_TOKEN')
+BOT_ID = os.getenv('BOT_ID')
+
+status_mapping = {
+    'SOLD': 'Продан',
+    'EXPIRED': 'Истекло время',
+    'TRADING': 'Идут торги',
+}
