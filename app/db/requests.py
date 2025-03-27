f0ad218039e5edb2ac1fe5dc3234afc714ce814e
qellyka <qellyka@proton.me>
2025-03-27 22:01:45 +0300

    Add new requests for auction's work

--- app/db/requests.py
+++ app/db/requests.py
@@ -2,7 +2,7 @@ import datetime
 from zoneinfo import ZoneInfo
 import uuid
 
-from sqlalchemy.util import await_only
+from datetime import datetime, timedelta
 
 from app.db.engine import async_session
 from app.db.models import UserBase, LotBase, LotStatus, LotModStatus
@@ -15,6 +15,7 @@ async def set_lot(tid: BigInteger, starter_price: int, hours_exp: int, pid: str,
         user = await session.scalar(
             select(UserBase)
             .where(UserBase.telegram_id==tid)
+            .with_for_update()
         )
         session.add(LotBase(
             user_id =user.id,
@@ -23,9 +24,9 @@ async def set_lot(tid: BigInteger, starter_price: int, hours_exp: int, pid: str,
             starter_price = starter_price,
             real_price = starter_price,
             moment_buy_price = blitz_price,
-            expired_at = datetime.datetime.now(ZoneInfo("Europe/Moscow")).replace(tzinfo=None) + datetime.timedelta(hours=hours_exp),
+            expired_at = datetime.now(ZoneInfo("Europe/Moscow")).replace(tzinfo=None) + timedelta(hours=hours_exp),
             expired_time = hours_exp,
-            seller = user.username,
+            seller = user.telegram_id,
             is_post = LotModStatus.PENDING,
             status = LotStatus.TRADING
         ))
@@ -37,6 +38,7 @@ async def deposit_balance(tg_id: BigInteger, stars: int):
         user = await session.scalar(
             select(UserBase)
             .where(UserBase.telegram_id==tg_id)
+            .with_for_update()
         )
         user.balance += stars
         await session.commit()
@@ -49,11 +51,11 @@ async def get_user_data(tg_id: BigInteger):
         )
         return  user
 
-async def get_user_data_id(tid: BigInteger):
+async def get_user_data_id(id: BigInteger):
     async with async_session() as session:
         user = await session.scalar(
             select(UserBase)
-            .where(UserBase.id==tid)
+            .where(UserBase.id==id)
         )
         return  user
 
@@ -82,16 +84,18 @@ async def approve_lot(lot_id: BigInteger):
         lot = await session.scalar(
             select(LotBase)
             .where(LotBase.id==lot_id)
+            .with_for_update()
         )
         lot.is_post = LotModStatus.APPROVED
-        lot.expired_at = datetime.datetime.now(ZoneInfo("Europe/Moscow")).replace(tzinfo=None) + datetime.timedelta(hours=lot.expired_time)
+        lot.expired_at = datetime.now(ZoneInfo("Europe/Moscow")).replace(tzinfo=None) + timedelta(hours=lot.expired_time)
         await session.commit()
 
-async def reject_lot(lot_id: BigInteger, tg_id: BigInteger):
+async def reject_lot(lot_id: BigInteger):
     async with async_session() as session:
         lot = await session.scalar(
             select(LotBase)
             .where(LotBase.id==lot_id)
+            .with_for_update()
         )
         lot.is_post = LotModStatus.REJECTED
         await session.commit()
@@ -118,6 +122,7 @@ async def ban_user(tid: BigInteger):
         user = await session.scalar(
             select(UserBase)
             .where(UserBase.telegram_id==tid)
+            .with_for_update()
         )
         user.is_banned = True
         await session.commit()
@@ -127,6 +132,7 @@ async def unban_user(tid: BigInteger):
         user = await session.scalar(
             select(UserBase)
             .where(UserBase.telegram_id==tid)
+            .with_for_update()
         )
         user.is_banned = False
         await session.commit()
@@ -162,12 +168,13 @@ async def get_first_new_lot():
 
 async def get_next_lot(current_lot_id):
     async with async_session() as session:
-        return await session.scalar(
+        lot =  await session.scalar(
             select(LotBase)
             .where(LotBase.is_post == LotModStatus.PENDING, LotBase.id > current_lot_id)
             .order_by(LotBase.id.asc())
             .limit(1)
         )
+        return lot
 
 async def get_previous_lot(current_lot_id):
     async with async_session() as session:
@@ -180,16 +187,18 @@ async def get_previous_lot(current_lot_id):
 
 async def get_lot_by_uuid(uuid: str):
     async with async_session() as session:
-        return await session.scalar(
+        lot = await session.scalar(
             select(LotBase)
             .where(LotBase.uuid==uuid)
         )
+        return lot
 
 async def set_lot_applicant(lot_id: int, applicant: int):
     async with async_session() as session:
         lot = await session.scalar(
             select(LotBase)
             .where(LotBase.id==lot_id)
+            .with_for_update()
         )
         lot.applicant = applicant
         await session.commit()
@@ -207,8 +216,9 @@ async def set_new_lot_price(lot_id: int, bid: int):
         lot = await session.scalar(
             select(LotBase)
             .where(LotBase.id==lot_id)
+            .with_for_update()
         )
-        lot.real_price = lot.real_price + bid
+        lot.real_price += bid
         await session.commit()
 
 async def set_message_id(lot_id: int, message_id: int):
@@ -216,6 +226,41 @@ async def set_message_id(lot_id: int, message_id: int):
         lot = await session.scalar(
             select(LotBase)
             .where(LotBase.id==lot_id)
+            .with_for_update()
         )
         lot.message_id = message_id
+        await session.commit()
+
+async def increase_balance(tg_id: BigInteger, bid: int):
+    async with async_session() as session:
+        user = await session.scalar(
+            select(UserBase)
+            .where(UserBase.telegram_id == tg_id)
+            .with_for_update()
+        )
+        print('USUS')
+        user.balance += bid
+        await session.commit()
+
+async def decrease_balance(tg_id: BigInteger, bid: int):
+    async with async_session() as session:
+        user = await session.scalar(
+            select(UserBase)
+            .where(UserBase.telegram_id == tg_id)
+            .with_for_update()
+        )
+        print('USUS')
+        user.balance -= bid
+        await session.commit()
+
+async def buy_now(lot_id, user_id):
+    async with async_session() as session:
+        lot = await session.scalar(
+                select(LotBase)
+                .where(LotBase.id==lot_id)
+                .with_for_update()
+        )
+        lot.status = LotStatus.SOLD
+        lot.buyer = user_id
+        lot.applicant = user_id
         await session.commit()
